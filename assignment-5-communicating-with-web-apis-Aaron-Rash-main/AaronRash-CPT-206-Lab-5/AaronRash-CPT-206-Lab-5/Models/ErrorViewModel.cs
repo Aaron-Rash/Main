6cb9e10c8793127448c5f27353373205b2227be3
Aaron Rash <155991638+Aaron-Rash@users.noreply.github.com>
2024-04-24 15:08:28 -0400

    Cleanup

--- assignment-5-communicating-with-web-apis-Aaron-Rash-main/AaronRash-CPT-206-Lab-5/AaronRash-CPT-206-Lab-5/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AaronRash_CPT_206_Lab_5.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
